145c9f05209078f28ef3768eac70c050aea33ec9
HusnulKhotami <151645826+HusnulKhotami@users.noreply.github.com>
2025-10-06 14:41:57 +0700

    Menyelesaikan langkah prak modul 5

--- app/Models/Mata_Kuliah.php
+++ app/Models/Mata_Kuliah.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Mata_Kuliah extends Model
 {
     use HasFactory;
+    protected $table = 'mata_kuliah';
+
+    protected $fillable = ['nama_mk','sks'];    
+    protected $guarded = ['id'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot(){
+        parent::boot();
+        static::creating(function($model){
+            if(empty($model->{$model->getKeyname()})){
+                $model->{$model->getKeyname()} = (string) Str::uuid();
+            }
+     });
+    }
+    public function getAllMK(){
+        return $this->all();
+    }
 }
